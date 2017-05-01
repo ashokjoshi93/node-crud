4061e4022147c0924a0d90a8baa36edf08d3d583
Ashok Joshi <joshiashok48684868@gmail.com>
2017-05-01 18:43:28 +0530

    Little error
    :wq

---
+++ app/controllers/events.controller.js
@@ -0,0 +1,17 @@
+module.exports={
+
+  showEvents:(req,res)=>{
+    var events=[
+      {name:'basketball',slug:'basketball',description:'Throwing into basket.'},
+      {name:'swimming',slug:'swimming',description:'Micheal Phelps vs Ian Thorpe.'},
+      {name:'weightlifting',slug:'weightlifting',description:'Lifting up really heavy things.'},
+    ];
+    res.render('pages/events',{events:events});
+  }
+
+  showSingle:(req,res)=>{
+    var event={name:'basketball',slug:'basketball',description:'Throwing into basket.'};
+    res.render('pages/single',{event:event});
+  }
+
+};
--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,9 @@
 var express=require('express');
 router=express.Router();
 mainController=require('./controllers/main.controller');
+eventsController=require('./controllers/events.controller');
 module.exports=router;
 
-router.get('/',mainController.showHome)
+router.get('/',mainController.showHome);
+router.get('/events',eventsController.showEvents);
+router.get('/events/:slug',eventsController.showSingle);
