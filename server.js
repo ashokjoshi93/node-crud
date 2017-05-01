6a3249130db953c3f4fd91a8a32de6ab53cb98ba
Ridham Tarpara <ridham.tarpara@gmail.com>
2017-05-01 13:33:38 +0530

    file loading issue resolved

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ var expressLayouts=require('express-ejs-layouts')
 var port=process.env.PORT||8080;
 
 
-app.use(require('./app/routes'));
 app.use(express.static(__dirname+'/public'));
 app.use(expressLayouts);
-
 app.set('view engine','ejs');
+
+
+app.use(require('./app/routes'));
 
 app.listen(port,()=>{
   console.log(`Now listening to localhost:${port}`);
